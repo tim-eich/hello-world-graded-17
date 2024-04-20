0f8ed9bd91a25567a11890dc361140e943213391
Tim-E <caffeinated.ink@gmail.com>
2024-04-20 15:28:32 -0400

    Better late than never?

--- src/main/java/org/launchcode/HelloWorld.java
+++ src/main/java/org/launchcode/HelloWorld.java
@@ -4,9 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(sayHello());
     }
-
     public static String sayHello() {
-        return "";
+        return "Hello, World!";
     }
 
 }
